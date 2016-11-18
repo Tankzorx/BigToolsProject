54296264382d95c03223e21a0548a0be80f4f1b9
Tankzorx <gustavj.dotzorx@gmail.com>
2016-11-19 00:02:02 +0100

    Created OnlineClassifier.py. An online classifier takes in a stream of data, instead of single chunk. DOES NOT READ CSV.(Reads the JSON format we have on our raw data.)

---
+++ OnlineClassifier.py
@@ -0,0 +1,115 @@
+import time
+startTick = time.time()
+import json
+import csv
+import re
+import codecs
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+import numpy as np
+# from imp import reload
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+DEBUG_ON = False
+def log(msg):
+	if DEBUG_ON:
+		print(msg)
+
+
+# If the file is fully read, next() calls will simply return the empty string.
+def readChunk(filename,lines_to_yield=1):
+	jsonObjects = []
+	lines_read = 0
+	with codecs.open(filename,encoding="utf-8") as fileHandle:
+		while True:
+			line = fileHandle.readline()
+			lines_read += 1
+			if line:
+				#print line
+				jsonObjects.append(line)
+				if lines_read >= lines_to_yield:
+					# We're here because we hit our line cap.
+					yield jsonObjects
+					jsonObjects = []
+					lines_read = 0
+			else:
+				# We're here because we hit EOF.
+				if len(jsonObjects) == 0:
+					return
+				else:
+					yield jsonObjects
+					jsonObjects = []
+					lines_read = 0
+
+def rangifyScore(score):
+	if score > 2000:
+		return 20
+	else:
+		# Floor-integer-division. :)
+		return score//50
+
+def main():
+	# Vectorizer with 2^18 buckets.
+	chunkSize = 100000
+	n_buckets = 2 ** 17
+
+	vectorizer = HashingVectorizer(decode_error='ignore', n_features=n_buckets,
+                               non_negative=True)
+	classifier = PassiveAggressiveClassifier()
+
+	#JSONGenerator = readChunk("data/dataSampleFile",chunkSize)
+	JSONGenerator = readChunk("data/RC_2009-12",chunkSize)
+
+	JSONArrayTestSet = next(JSONGenerator)
+	X_test_text = []
+	Y_test = []
+	for JSONString in JSONArrayTestSet:
+		JSONObject = json.loads(JSONString)
+		# Don't care about deleted content.
+		if JSONObject["body"] == "[deleted]":
+			continue
+
+		X_test_text.append(JSONObject["body"])
+		Y_test.append(rangifyScore(int(JSONObject["score"])))
+
+	X_test = vectorizer.transform(X_test_text)
+	log("Start till MainLoop timer: " + str(time.time() - startTick))
+	generatorTimeTick = time.time()
+	# For loop for generators. Smart!
+	for i, JSONArray in enumerate(JSONGenerator):
+		log("readChunkTimer: " + str(time.time() - generatorTimeTick))
+		
+
+		X_train_text = []
+		Y_train = []
+		extractFeatureTimeTick = time.time()
+		for JSONString in JSONArray:
+
+			JSONObject = json.loads(JSONString)
+			# Don't care about deleted content.
+			if JSONObject["body"] == "[deleted]":
+				continue
+
+			X_train_text.append(JSONObject["body"])
+			Y_train.append(rangifyScore(int(JSONObject["score"])))
+
+		log("Feature Extract timer: " + str(time.time() - extractFeatureTimeTick))
+		tick = time.time()
+		X_train = vectorizer.transform(X_train_text)
+		log("Vectorize timer:" + str(time.time() - tick))
+
+		tick = time.time()
+		classifier.partial_fit(X_train,Y_train,classes=[i for i in range(41)])
+		log("Partial fit timer:" + str(time.time() - tick))
+
+		generatorTimeTick = time.time()
+
+
+	print(classifier.score(X_test,Y_test))
+
+
+
+if __name__ == '__main__':
+	main()
